c3e867a110209bd532b3eacad51b67f6093b0a4c
Ogwuru Patrick <sirpatrickchigozie@gmail.com>
2022-03-21 06:50:00 +0100

    Made significant updates to the existing UI of page

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -4,7 +4,7 @@
       class="fixed top-0 flex flex-row items-center justify-between w-full h-16 px-12 py-2 bg-gray-900"
     >
       <span class="home_links" id="header_logo">
-        <h1 class="text-xl">Logo</h1>
+        <img src="../assets/ME.jpg" class="w-10 h-10 rounded-full" alt="" />
       </span>
       <ul v-show="show" id="header_links">
         <li class="text-gray-200 links" id="home"><a href="">Home</a></li>
@@ -51,17 +51,18 @@
       <div class="main">
         <!-- intro -->
         <div class="text-gray-200 shadow-md content" id="intro">
-          <h2 class="content_name">I'm Noble d Noobie</h2>
+          <h2 class="content_name">I'm Ogwuru Patrick</h2>
           <p>
             Also known as <i>Mod5ied</i> in the tech space, I'm a self taught
             JavaScript developer, technical writer and founder of IboTran.io. I
-            help founders craft software solutions using JavaScript tools. I'm
-            also an active opensourcer, contributing and also giving out to the
-            community wih my creative ideas.
+            help founders craft software solutions using modern software
+            development tools like JavaScript, Nuxt.Js and Vue.js.. I'm also an
+            active in OSS, contributing and also giving out to the community wih
+            my creative ideas.
           </p>
           <span>
             <button
-              class="flex flex-row justify-center gap-2 text-gray-300 bg-orange-900 border-2 border-orange-900 shadow-xl btnOne"
+              class="flex flex-row justify-center gap-2 rounded-lg hover:bg-gray-300 hover:text-orange-800 text-gray-300 bg-orange-900 border-2 border-orange-900 shadow-xl transition duration-200 btnOne"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -79,8 +80,9 @@
               </svg>
               Resume
             </button>
-            <button
-              class="flex flex-row justify-center gap-2 text-gray-300 bg-orange-900 border-2 border-orange-900 shadow-xl btnOne"
+            <a
+              href="#contact"
+              class="flex flex-row justify-center gap-2 rounded-lg text-gray-300 bg-orange-900 border-2 border-orange-900 shadow-xl btnOne"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -97,32 +99,54 @@
                 />
               </svg>
               Contact
-            </button>
+            </a>
           </span>
         </div>
         <!-- skills -->
         <div class="text-gray-200 shadow-md content" id="skills">
-          <h3>Skills</h3>
-          <ul id="skills_list" class="h-full">
+          <h3>Skills and Stack -> {MEVN, PEVN}</h3>
+          <ul id="skills_list" class="h-full flex flex-col md:flex-row">
             <li
               id="list"
-              class="flex flex-col items-center justify-between h-full p-2"
+              class="flex flex-col items-center justify-between h-full p-2 relative transition duration-70000"
             >
               <i> <img src="../assets/engineering.svg" alt="" class="" /> </i>
 
-              <p class="p-2 mb-4 text-gray-300 bg-orange-800 rounded-lg">
+              <p
+                @click="tag1 = !tag1"
+                class="px-4 py-2 mb-4 text-gray-300 bg-orange-800 rounded-lg cursor-pointer text-xs md:text-base tags"
+              >
                 Frontend Development
+              </p>
+              <p
+                class="bg-indigo-700 absolute px-2 rounded md:h-20 text-xs md:text-sm"
+                v-if="tag1"
+              >
+                I work with these technologies on the frontend: <br />
+                Vue.Js, TypeScript, JavaScript, MongoDB, HTML5, CSS3, SASS/SCSS
+                & TailwindCSS
               </p>
             </li>
             <li
               id="list"
-              class="flex flex-col items-center justify-between h-full p-2"
+              class="flex flex-col items-center justify-between h-full p-2 relative"
             >
               <i>
                 <img src="../assets/frontend.svg" alt="" class="h-3/4" />
               </i>
-              <p class="p-2 mb-4 text-gray-300 bg-indigo-700 rounded-lg">
+              <p
+                @click="tag2 = !tag2"
+                class="px-4 py-2 mb-4 text-gray-300 bg-indigo-700 rounded-lg cursor-pointer text-xs md:text-base tags"
+              >
                 Backend Development
+              </p>
+              <p
+                class="bg-gray-500 absolute px-4 rounded text-xs md:text-sm md:h-20"
+                v-if="tag2"
+              >
+                I'm proficient with Backend technologies like: <br />
+                Nuxt.Js, Node.Js, Express, TypeScript with Node, GraphQL,
+                Apollo, MongoDB and PostgreSQL
               </p>
             </li>
           </ul>
@@ -133,7 +157,7 @@
           <ul id="work_list">
             <li class="list">
               <p
-                class="absolute p-2 mb-4 text-gray-300 bg-indigo-700 rounded-lg"
+                class="absolute px-4 py-2 mb-4 text-sm text-gray-300 bg-indigo-700 rounded-lg"
               >
                 Easybank landing page
               </p>
@@ -145,7 +169,7 @@
             </li>
             <li class="list">
               <p
-                class="absolute p-2 mb-4 text-gray-300 bg-indigo-700 rounded-lg"
+                class="absolute px-4 py-2 mb-4 text-sm text-gray-300 bg-indigo-700 rounded-lg"
               >
                 Clearon - Clearance app
               </p>
@@ -157,7 +181,7 @@
             </li>
             <li class="list">
               <p
-                class="absolute p-2 mb-4 text-gray-300 bg-indigo-700 rounded-lg"
+                class="absolute px-4 py-2 mb-4 text-sm text-gray-300 bg-indigo-700 rounded-lg"
               >
                 NotesHub
               </p>
@@ -169,7 +193,7 @@
             </li>
             <li class="list">
               <p
-                class="absolute p-2 mb-4 text-gray-300 bg-indigo-700 rounded-lg"
+                class="absolute px-4 py-2 mb-4 text-sm text-gray-300 bg-indigo-700 rounded-lg"
               >
                 Notify - Notification app
               </p>
@@ -184,30 +208,32 @@
         </div>
         <!-- services -->
         <div class="gap-16 text-gray-200 shadow-md content" id="services">
-          <h3>Services</h3>
-          <ul id="service_list" class="flex flex-row gap-16 rounded-md">
-            <span class="flex flex-col items-center gap-2">
-              <li class="text-lg list">Frontend Engineering</li>
-              <p class="text-sm">
+          <h3>Services Rendered</h3>
+          <ul id="service_list" class="flex flex-row gap-16 rounded-md h-60">
+            <span class="flex flex-col gap-2">
+              <li class="text-lg bg-gray-500 list">Frontend Engineering</li>
+              <p class="text-sm p-4">
                 I craft and develop interesting and intruiging web applications
                 and platforms using various web technologies in my stack: HTML,
                 CSS, JavaScript, Vue.js and Nuxt
               </p>
             </span>
             <span class="flex flex-col gap-2">
-              <li class="text-lg list">Backend/API development</li>
-              <p class="text-sm">
+              <li class="text-lg bg-orange-900 list">
+                Backend/API development
+              </li>
+              <p class="text-sm p-4">
                 Being very proficient in the art of building RESTful application
                 services is a skill I have developed and honed over the years. I
-                also love working with and designing GraphQL APIs with MongoDB
-                and Mongoose. <br />
-                Security is most assured with SuperTokens - Yes, I am a
+                also love working with and designing GraphQL APIs with Apollo
+                and MongoDb. <br />
+                Security is assured with SuperTokens - Yes, I am a
                 <a href="">SuperTokens</a> ambassador!
               </p>
             </span>
             <span class="flex flex-col gap-2">
-              <li class="text-lg list">Technical writing</li>
-              <p class="text-sm">
+              <li class="text-lg bg-indigo-700 list">Technical writing</li>
+              <p class="text-sm p-4">
                 With practically tested codes and programs, I formulate
                 technical articles that also includes my hours of research to
                 help ease dev experience. I write on Dev.to and Tealfield.
@@ -218,40 +244,79 @@
         <!-- opensource -->
         <div class="text-gray-200 shadow-md content" id="opensource">
           <h3>OpenSource</h3>
-          <ul id="open_list">
+          <ul
+            id="open_list"
+            class="flex flex-wrap justify-around rounded-md p-2 gap-16 w-4/5 border border-gray-700"
+          >
             <li class="list">SignIn/Up page</li>
+
             <li class="list">AutoComplete component</li>
-            <li class="list">My Portfolio</li>
-            <li class="list">IboTrans.io</li>
           </ul>
         </div>
         <!-- articles -->
         <div class="text-gray-200 shadow-md content" id="articles">
           <h3>Popular Articles</h3>
-          <ul id="articles_list">
-            <li class="article">Req/Res</li>
-            <li class="article">Unsplash</li>
-            <li class="article">Unsplash2</li>
-            <li class="article">EventLoop</li>
+          <ul
+            id="articles_list"
+            class="flex flex-col justify-around gap-8 w-4/5 p-2"
+          >
+            <li
+              class="h-10 w-3/4 px-4 py-2 text-sm text-gray-300 bg-indigo-700 rounded-lg hover:w-3/5 article"
+            >
+              Introduction to Request and Response in JavaScript
+            </li>
+            <li
+              class="h-10 w-3/5 px-4 py-2 ml-auto text-sm text-gray-300 bg-orange-700 rounded-lg hover:w-3/4 article"
+            >
+              Intro to making API requests (Unsplash API)
+            </li>
+            <li
+              class="h-10 w-3/5 px-4 py-2 text-sm text-gray-300 bg-indigo-700 rounded-lg hover:w-3/4 article"
+            >
+              Intro to making API requests (Unsplash API)2
+            </li>
+            <li
+              class="h-10 w-3/4 px-4 py-2 ml-auto text-sm text-gray-300 bg-orange-700 rounded-lg hover:w-3/5 article"
+            >
+              Node.Js EventLoop Demystified
+            </li>
           </ul>
           <a href="" class="mr-auto">See more..</a>
         </div>
         <!-- contact -->
-        <div class="text-gray-200 shadow-md content" id="contact">
+        <div
+          class="text-gray-200 shadow-md flex my-auto mx-0 h-3 content"
+          id="contact"
+        >
           <h3 class="text-gray-200">Reach out here</h3>
           <ul
             id="contact_list"
-            class="flex flex-row justify-between w-2/5 mx-0 my-auto"
+            class="flex flex-row gap-4 justify-between w-2/5 mx-0 my-auto"
           >
-            <li class="list">Twitter</li>
-            <li class="list">LinkedIn</li>
-            <li class="list">Github</li>
+            <a
+              href="#"
+              class="list w-3/5 p-2 hover:bg-blue-700 hover:text-gray-200 border border-gray-700 transition duration-300"
+            >
+              Twitter
+            </a>
+            <a
+              href="#"
+              class="list w-3/5 p-2 hover:bg-blue-500 hover:text-gray-300 border border-gray-600 transition duration-300"
+            >
+              LinkedIn
+            </a>
+            <a
+              href="#"
+              class="list w-3/5 p-2 hover:bg-gray-300 hover:text-gray-700 border border-gray-600 transition duration-300"
+            >
+              Github
+            </a>
           </ul>
         </div>
       </div>
       <!-- The Right side navigation -->
-      <section id="main_nav_container" @mousemove="removeAnimateClass">
-        <ul id="nav_links">
+      <!-- <section id="main_nav_container" @mousemove="removeAnimateClass">
+        <ul id="nav_links" class="px-4">
           <li class="links" id="home">
             <a class="text-gray-200" href="#">Home</a>
             <fulfilling-bouncing-circle-spinner
@@ -293,7 +358,7 @@
             />
           </li>
         </ul>
-      </section>
+      </section> -->
     </main>
     <footer class="static bottom-0 flex flex-col w-full mt-4">
       <ul
@@ -301,7 +366,7 @@
         class="flex flex-row justify-around p-2 text-gray-200 bg-gray-900"
       >
         <li id="home" class="relative mx-auto my-0 right-80">
-          <a href="#">Logo</a>
+          <img src="../assets/ME.jpg" class="w-8 h-8 rounded-full" alt="" />
         </li>
         <span id="foot_nav" class="relative flex flex-row gap-6 right-1/3">
           <li class="links" id="about"><a href="">About</a></li>
@@ -322,7 +387,7 @@
       </ul>
       <span
         id="foot_attrib"
-        class="flex flex-row justify-center w-full gap-3 p-2 text-gray-300 bg-orange-900"
+        class="flex flex-row justify-center w-full gap-3 p-2 text-gray-300 bg-orange-700"
       >
         <p>&copy; Patrick 2022</p>
         <p class="flex items-center gap-1">
@@ -347,6 +412,8 @@ import { reactive, ref, toRefs } from "@vue/reactivity";
 import "animate.css";
 import { FulfillingBouncingCircleSpinner } from "epic-spinners";
 
+const tag1 = ref(false);
+const tag2 = ref(false);
 const show = ref(false);
 const methods = reactive({
   removeAnimateClass: () => {
