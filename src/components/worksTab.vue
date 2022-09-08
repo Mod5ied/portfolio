f9e0a07063a9a3ad7298db74ee98f36a88fb2155
Ogwuru Patrick <sirpatrickchigozie@gmail.com>
2022-09-08 22:51:41 +0100

    added portfolio link

--- src/components/worksTab.vue
+++ src/components/worksTab.vue
@@ -21,7 +21,7 @@
                                 d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"></path>
                         </svg>
                     </a>
-                    <a href="" class="worksTabLinks">
+                    <a href="https://ogwurup.vercel.app/" class="worksTabLinks">
                         <p class="worksTabParas"> My Portfolio - (OpenSourced)</p>
                         <svg class="w-3 h-3 text-indigo-600 md:w-5 md:h-5 hover:text-indigo-400 dark:text-slate-300" fill="none" stroke="currentColor"
                             viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
